c4c25c9fb10ffa1e32500eacb32271ef37cbf90a
matthewpdias <matthewpdias@gmail.com>
2017-06-08 13:11:27 -0700

    stripped out boilerplate functionality

--- jenkins/jobs/dsl/skeleton_pipeline_jobs.groovy
+++ jenkins/jobs/dsl/skeleton_pipeline_jobs.groovy
@@ -10,8 +10,7 @@ def projectScmNamespace = "${SCM_NAMESPACE}"
 
 // Variables
 // **The git repo variables will be changed to the users' git repositories manually in the Jenkins jobs**
-def skeletonAppgitRepo = "https://github.com/matthewpdias/spring-petclinic.git"
-def regressionTestGitRepo = "YOUR_REGRESSION_TEST_REPO"
+def testGitRepo = "https://github.com/matthewpdias/spring-petclinic.git"
 
 // ** The logrotator variables should be changed to meet your build archive requirements
 def logRotatorDaysToKeep = 7
@@ -20,31 +19,18 @@ def logRotatorArtifactsNumDaysToKeep = 7
 def logRotatorArtifactsNumToKeep = 7
 
 // Jobs
-def buildAppJob = freeStyleJob(projectFolderName + "/Skeleton_Application_Build")
-def unitTestJob = freeStyleJob(projectFolderName + "/Skeleton_Application_Unit_Tests")
-def codeAnalysisJob = freeStyleJob(projectFolderName + "/Skeleton_Application_Code_Analysis")
-def deployJob = freeStyleJob(projectFolderName + "/Skeleton_Application_Deploy")
-def regressionTestJob = freeStyleJob(projectFolderName + "/Skeleton_Application_Regression_Tests")
+def testJob = freeStyleJob(projectFolderName + "/Skeleton_Test_Job")
 
 // Views
-def pipelineView = buildPipelineView(projectFolderName + "/Skeleton_Application")
 
-pipelineView.with{
-    title('Skeleton Application Pipeline')
-    displayedBuilds(5)
-    selectedJob(projectFolderName + "/Skeleton_Application_Build")
-    showPipelineParameters()
-    showPipelineDefinitionHeader()
-    refreshFrequency(5)
-}
 
 // All jobs are tied to build on the Jenkins slave
 // The functional build steps for each job have been left empty
 // A default set of wrappers have been used for each job
 // New jobs can be introduced into the pipeline as required
 
-buildAppJob.with{
-  description("Skeleton application build job.")
+testJob.with{
+  description("cartridge skeleton test job.")
   logRotator {
     daysToKeep(logRotatorDaysToKeep)
     numToKeep(logRotatorBuildNumToKeep)
@@ -56,7 +42,6 @@ buildAppJob.with{
       env('WORKSPACE_NAME',workspaceFolderName)
       env('PROJECT_NAME',projectFolderName)
   }
-  label("docker")
   wrappers {
     preBuildCleanup()
     injectPasswords()
@@ -65,168 +50,11 @@ buildAppJob.with{
   }
   triggers scmProvider.trigger(projectScmNamespace, skeletonAppgitRepo, "master")
   steps {
-    shell('''## YOUR BUILD STEPS GO HERE'''.stripMargin())
-  }
-  publishers{
-    downstreamParameterized{
-      trigger(projectFolderName + "/Skeleton_Application_Unit_Tests"){
-        condition("UNSTABLE_OR_BETTER")
-        parameters{
-          predefinedProp("B",'${BUILD_NUMBER}')
-          predefinedProp("PARENT_BUILD", '${JOB_NAME}')
-        }
-      }
-    }
-  }
-}
-
-unitTestJob.with{
-  description("This job runs unit tests on our skeleton application.")
-  parameters{
-    stringParam("B",'',"Parent build number")
-    stringParam("PARENT_BUILD","Skeleton_Application_Build","Parent build name")
-  }
-  logRotator {
-    daysToKeep(logRotatorDaysToKeep)
-    numToKeep(logRotatorBuildNumToKeep)
-    artifactDaysToKeep(logRotatorArtifactsNumDaysToKeep)
-    artifactNumToKeep(logRotatorArtifactsNumToKeep)
-  }
-  wrappers {
-    preBuildCleanup()
-    injectPasswords()
-    maskPasswords()
-    sshAgent("adop-jenkins-master")
-  }
-  environmentVariables {
-      env('WORKSPACE_NAME',workspaceFolderName)
-      env('PROJECT_NAME',projectFolderName)
-  }
-  label("docker")
-  steps {
-  }
-  steps {
-    shell('''## YOUR UNIT TESTING STEPS GO HERE'''.stripMargin())
-  }
-  publishers{
-    downstreamParameterized{
-      trigger(projectFolderName + "/Skeleton_Application_Code_Analysis"){
-        condition("UNSTABLE_OR_BETTER")
-        parameters{
-          predefinedProp("B",'${B}')
-          predefinedProp("PARENT_BUILD",'${PARENT_BUILD}')
-        }
-      }
-    }
-  }
-}
-
-codeAnalysisJob.with{
-  description("This job runs code quality analysis for our skeleton application using SonarQube.")
-  parameters{
-    stringParam("B",'',"Parent build number")
-    stringParam("PARENT_BUILD","Skeleton_Application_Build","Parent build name")
-  }
-  logRotator {
-    daysToKeep(logRotatorDaysToKeep)
-    numToKeep(logRotatorBuildNumToKeep)
-    artifactDaysToKeep(logRotatorArtifactsNumDaysToKeep)
-    artifactNumToKeep(logRotatorArtifactsNumToKeep)
-  }
-  environmentVariables {
-      env('WORKSPACE_NAME',workspaceFolderName)
-      env('PROJECT_NAME',projectFolderName)
-  }
-  wrappers {
-    preBuildCleanup()
-    injectPasswords()
-    maskPasswords()
-    sshAgent("adop-jenkins-master")
-  }
-  label("docker")
-  steps {
-    shell('''## YOUR CODE ANALYSIS STEPS GO HERE'''.stripMargin())
-  }
-  publishers{
-    downstreamParameterized{
-      trigger(projectFolderName + "/Skeleton_Application_Deploy"){
-        condition("UNSTABLE_OR_BETTER")
-        parameters{
-          predefinedProp("B",'${B}')
-          predefinedProp("PARENT_BUILD", '${PARENT_BUILD}')
-        }
-      }
-    }
-  }
-}
-
-deployJob.with{
-  description("This job deploys the skeleton application to the CI environment")
-  parameters{
-    stringParam("B",'',"Parent build number")
-    stringParam("PARENT_BUILD","Skeleton_Application_Build","Parent build name")
-    stringParam("ENVIRONMENT_NAME","CI","Name of the environment.")
-  }
-  logRotator {
-    daysToKeep(logRotatorDaysToKeep)
-    numToKeep(logRotatorBuildNumToKeep)
-    artifactDaysToKeep(logRotatorArtifactsNumDaysToKeep)
-    artifactNumToKeep(logRotatorArtifactsNumToKeep)
-  }
-  wrappers {
-    preBuildCleanup()
-    injectPasswords()
-    maskPasswords()
-    sshAgent("adop-jenkins-master")
-  }
-  environmentVariables {
-      env('WORKSPACE_NAME',workspaceFolderName)
-      env('PROJECT_NAME',projectFolderName)
-  }
-  label("docker")
-  steps {
-    shell('''## YOUR DEPLOY STEPS GO HERE'''.stripMargin())
-  }
-  publishers{
-    downstreamParameterized{
-      trigger(projectFolderName + "/Skeleton_Application_Regression_Tests"){
-        condition("UNSTABLE_OR_BETTER")
-        parameters{
-          predefinedProp("B",'${B}')
-          predefinedProp("PARENT_BUILD", '${PARENT_BUILD}')
-          predefinedProp("ENVIRONMENT_NAME", '${ENVIRONMENT_NAME}')
-        }
-      }
-    }
-  }
-}
-
-regressionTestJob.with{
-  description("This job runs regression tests on the deployed skeleton application")
-  parameters{
-    stringParam("B",'',"Parent build number")
-    stringParam("PARENT_BUILD","Skeleton_Application_Build","Parent build name")
-    stringParam("ENVIRONMENT_NAME","CI","Name of the environment.")
-  }
-  logRotator {
-    daysToKeep(logRotatorDaysToKeep)
-    numToKeep(logRotatorBuildNumToKeep)
-    artifactDaysToKeep(logRotatorArtifactsNumDaysToKeep)
-    artifactNumToKeep(logRotatorArtifactsNumToKeep)
-  }
-  scm scmProvider.get(projectFolderName, regressionTestGitRepo, "*/master", "adop-jenkins-master", null)
-  wrappers {
-    preBuildCleanup()
-    injectPasswords()
-    maskPasswords()
-    sshAgent("adop-jenkins-master")
-  }
-  environmentVariables {
-      env('WORKSPACE_NAME',workspaceFolderName)
-      env('PROJECT_NAME',projectFolderName)
-  }
-  label("docker")
-  steps {
-    shell('''## YOUR REGRESSION TESTING STEPS GO HERE'''.stripMargin())
+    shell('''
+    |#An execute shell step, just for example purposes
+    |
+    |echo This job was cartridge loaded!
+    |
+    |'''.stripMargin())
   }
 }
